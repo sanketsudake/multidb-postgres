0bab3017a0f2cc2bbdd62de32e7b4caf227ca074
Sanket Sudake <sanket@infracloud.io>
2018-10-29 12:14:13 +0530

    Add support to turn SSL on/off

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-multidb-postgres
-==================
+multidb-postgres-ssl
+====================
 
 [Automated Build](https://hub.docker.com/r/tripples/multidb-postgres/)
 
@@ -7,7 +7,12 @@ multidb-postgres
 This postgres image facilitates quick script which can create multiple databases on postgres start.
 Image targets usecase, where multiple applications are hosted through docker-compose and share single PG container.
 
-Specify the following environment variable with comma separated database names to be created,
+Supported configuration parameters:
+
+*  Databases to be created,
 
 `INIT_DATABASES=test1,test2,test3`
+
+* Turn SSL on/off
+`SSL_MODE=on` or `SSL_MODE=off`
 
--- init-db.sh
+++ init-db.sh
@@ -5,10 +5,23 @@ if [ -z "${INIT_DATABASES}" ] ; then
     echo "INIT_DATABASES variable not set"
     exit 1
 fi
-
 echo "INIT_DATABASES value", ${INIT_DATABASES}
 
+cd "${PGDATA}"
+cp /etc/ssl/certs/ssl-cert-snakeoil.pem "${PGDATA}"/server.crt
+cp /etc/ssl/private/ssl-cert-snakeoil.key "${PGDATA}"/server.key
+chmod og-rwx server.key
+chown -R postgres:postgres "${PGDATA}"
 
+# Turn on/off ssl
+if [ -z "${SSL_MODE}" ] ; then
+    SSL_MODE=off
+fi
+echo "SSL is ${SSL_MODE}"
+
+sed -ri "s/^#?(ssl\s*=\s*)\S+/\1'${SSL_MODE}'/" "$PGDATA/postgresql.conf"
+
+# Create user mentioned databases and grant permissions
 for db in `echo ${INIT_DATABASES} | tr ',' '\n'`
 do
     echo "CREATE DATABASE $db;" >> /tmp/initdb.sql
